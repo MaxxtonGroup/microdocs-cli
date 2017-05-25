953eece0a0f96e7958db3e4bfa156c1079983ec4
Rob Sonke <rob@tigrou.nl>
2017-05-25 14:26:23 +0200

    Improved postman cli

--- src/cli/cli.export.ts
+++ src/cli/cli.export.ts
@@ -5,10 +5,9 @@ import { Project, ProblemResponse } from "@maxxton/microdocs-core/domain";
 import * as cliHelper from '../helpers/cli.helper';
 import { ServerOptions } from "../options/server.options";
 import { CheckOptions } from "../options/check.options";
-import * as fs from 'fs';
-import * as uuid from 'uuid';
-import * as pathUtil from 'path';
+
 import { SwaggerAdapter, PostmanAdapter, BaseAdapter } from '@maxxton/microdocs-core/adapter';
+
 
 /**
  * Run like: microdocs export --format swagger --swaggerFile
@@ -28,7 +27,7 @@ module.exports = new Command("export")
       adapter = new SwaggerAdapter();
     }
     else if (args.options['format'] == 'postman') {
-      adapter = new MyPostmanAdapter();
+      adapter = new PostmanAdapter();
     }
     else {
       reject("Format is unknown, use: swagger or postman");
@@ -37,51 +36,9 @@ module.exports = new Command("export")
     // convert the definition
     let result: any = adapter.adapt(project);
 
-    if (args.options['format'] == 'swagger') {
-      let swaggerFile = args.options.swaggerFile;
-      if (!swaggerFile) {
-        swaggerFile = 'swagger.json';
-      }
-
-      let swaggerFolder = pathUtil.dirname(swaggerFile);
-      const mkdirp = require('mkdirp');
-      mkdirp.sync(swaggerFolder);
-      logger.info(`Store swagger definition in '${swaggerFile}'`);
-      let json = JSON.stringify(project);
-      fs.writeFileSync(swaggerFile, json);
-    }
-    else if (args.options['format'] == 'postman') {
-      let postmanFile = args.options.postmanFile;
-      if (!postmanFile) {
-        postmanFile = 'postman.json';
-      }
-
-      let postmanFolder = pathUtil.dirname(postmanFile);
-      const mkdirp = require('mkdirp');
-      mkdirp.sync(postmanFolder);
-      logger.info(`Store swagger definition in '${postmanFile}'`);
-      let json = JSON.stringify(result);
-      fs.writeFileSync(postmanFile, json);
+    if (args.options['format'] == 'swagger' || args.options['format'] == 'postman') {
+      cliHelper.storeResult(result, args.options['format'], args.options.swaggerFile);
     }
 
     logger.info("Export to " + args.options['format'] + " definition succeed");
   });
-
-class MyPostmanAdapter extends PostmanAdapter {
-  adapt(project: Project): {}[] {
-    return super.adapt(project);
-  }
-
-  getPostmanBase(project?: Project): {} {
-    var collection: any = { item: [], info: {} };
-    collection['info'] = {
-      name: project.info.title,
-      version: project.info.version,
-      description: project.info.description
-    };
-    collection.info.schema = "https://schema.getpostman.com/json/collection/v2.0.0/collection.json";
-    collection.info._postman_id = uuid['v4']();
-
-    return collection;
-  }
-}
